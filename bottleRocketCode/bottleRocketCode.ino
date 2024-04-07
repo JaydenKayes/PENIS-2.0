e85913d061adc74d3f96587d14d9d8b0fd0bdc83
David <56525307+Mr-Hansen-Orewa@users.noreply.github.com>
2024-04-07 13:19:31 +1200

    Update bottleRocketCode.ino
    teacher notes added

--- bottleRocketCode/bottleRocketCode.ino
+++ bottleRocketCode/bottleRocketCode.ino
@@ -3,6 +3,7 @@
 #include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
 #include <SparkFun_LPS25HB_Arduino_Library.h> // Click here to get the library: http://librarymanager/All#SparkFun_LPS25HB
 
+//TEACHER COMMENTS so what do these lines of code do?
 LPS25HB bottleSensor; // Create an object of the LPS25HB class
 OpenLog bottleReadings; //Create instance
 
@@ -13,17 +14,15 @@ void setup()
   bottleSensor.begin(); // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
   bottleReadings.begin(); //Open connection to OpenLog (no pun intended)
 
-  if (bottleSensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+//TEACHER COMMENTS what is this checking again?  
+  if (bottleSensor.isConnected() == false) 
   {
     Serial.println("theres a problem");
     while (1);
   }
 
-
-
-
-  Serial.println("Run OpenLog Append File Test");
-
+  Serial.println("Run OpenLog Append File Test"); //TEACHER COMMENTS is this from the example code? do you need it?
+//TEACHER COMMENTS what is the name of your file going to be? 
   bottleReadings.append("appendMe.txt");
   bottleReadings.println("This is recorded to appendMe.txt");
 
@@ -35,10 +34,13 @@ void setup()
 
 void loop()
 {
+  //TEACHER COMMENTS what is this chunk of code doing?
   Serial.print("Pressure in hPa: ");
   Serial.print(bottleSensor.getPressure_hPa()); // Get the pressure reading in hPa
   Serial.print(", Temperature (degC): ");
   Serial.println(bottleSensor.getTemperature_degC()); // Get the temperature in degrees C
 
+  //TEACHER COMMENTS wouldn't it be great if your bottleReadings and bottleSensor did something here? 
+  
   delay(40); // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
 }
